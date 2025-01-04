999b72c7a1b76b5d125e20ed604c260aaeaae8a9
Георгий Шельгаас <shelgaas00@mail.ru>
2025-01-04 17:48:17 +0300

    Поправить код стайл

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,10 +1,11 @@
 package manager;
 
 public class Managers {
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
-    };
-    public static HistoryManager getDefaultHistory(){
+    }
+
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
-    };
+    }
 }
