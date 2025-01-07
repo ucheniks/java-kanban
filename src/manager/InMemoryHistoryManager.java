47619b0281069abd85f5c80e8fcc2e7b9cdbe40b
Георгий Шельгаас <shelgaas00@mail.ru>
2025-01-07 18:31:40 +0300

    Упростить методы получения и удаления

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -8,7 +8,6 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     private Node head;
     private Node tail;
-    private int size;
     private final Map<Integer, Node> historyManager;
 
     public InMemoryHistoryManager() {
@@ -24,64 +23,52 @@ public class InMemoryHistoryManager implements HistoryManager {
         } else {
             oldTail.next = newNode;
         }
-        size++;
         historyManager.put(element.getId(), newNode);
     }
 
     private List<Task> getTasks() {
         List<Task> listOfTasks = new ArrayList<>();
         Node currentHead = head;
-        int size = 0;
-        if (head.data == tail.data) {
-            listOfTasks.add(tail.data);
-            return listOfTasks;
-        }
-        while (size < this.size) {
-            listOfTasks.add(currentHead.data);
+        listOfTasks.add(head.data);
+        while (currentHead.data != null && currentHead.data != tail.data) {
             currentHead = currentHead.next;
-            size++;
+            listOfTasks.add(currentHead.data);
         }
         return listOfTasks;
     }
 
     private void removeNode(Node node) {
-        if (node.equals(head) && size == 2) {
-            tail.prev = null;
-            head = tail;
-        } else if (node.equals(tail) && size == 2) {
-            head.next = null;
-            tail = head;
-        } else if (node.equals(tail)) {
-            Node oldTail = tail;
-            Node newNode = new Node(oldTail.prev.prev, oldTail.prev.data, null);
-            tail = newNode;
-            oldTail.prev.prev.next = newNode;
+        if (node.data == head.data && node.data == tail.data) {
+            head = null;
+            tail = null;
         } else {
-            node.data = node.next.data;
-            if (node.data == tail.data) {
-                tail = node;
-                tail.next = null;
-                tail.prev.next = tail;
+            if (node.equals(head)) {
+                node.next.prev = null;
+                head = node.next;
+                historyManager.put(head.data.getId(), head);
             } else {
-                node.next = node.next.next;
-                node.next.prev = node;
+                if (node.equals(tail)) {
+                    node.prev.next = null;
+                    tail = node.prev;
+                    historyManager.put(tail.data.getId(), tail);
+                } else {
+                    node.prev.next = node.next;
+                    node.next.prev = node.prev;
+                    historyManager.put(node.prev.data.getId(), node.prev);
+                    historyManager.put(node.next.data.getId(), node.next);
+                }
             }
         }
-
-        size--;
-        historyManager.put(node.data.getId(), node);
     }
 
 
     @Override
     public void add(Task task) {
         Node nodeInHistory = historyManager.get(task.getId());
-        if (historyManager.containsKey(task.getId()) && !nodeInHistory.equals(tail) && size != 1) {
+        if (historyManager.containsKey(task.getId())) {
             removeNode(nodeInHistory);
-            linkLast(task);
-        } else if (!historyManager.containsKey(task.getId())) {
-            linkLast(task);
         }
+        linkLast(task);
     }
 
     @Override
@@ -97,12 +84,12 @@ public class InMemoryHistoryManager implements HistoryManager {
         return getTasks();
     }
 
-    class Node {
-        public Task data;
-        public Node next;
-        public Node prev;
+    private static class Node {
+        private final Task data;
+        private Node next;
+        private Node prev;
 
-        public Node(Node prev, Task data, Node next) {
+        private Node(Node prev, Task data, Node next) {
             this.data = data;
             this.next = next;
             this.prev = prev;
